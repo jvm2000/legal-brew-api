2d0df5385a3da802366415086c8b5e01a54c6aa3
jvm2000 <minozajohnvincent2000@gmail.com>
2025-07-10 18:42:30 +0800

    fixed: authentication

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,14 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['http://localhost:3000'],  // your Nuxt 3 app
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
+    'allowed_methods' => ['*'],
+    'exposed_headers' => ['*'],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AuthController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
+
