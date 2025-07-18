0083b0820da9f942e67458be8854a3a021f13aac
jvm2000 <minozajohnvincent2000@gmail.com>
2025-07-18 20:26:10 +0800

    fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'abilities' => CheckAbilities::class,
-            'ability' => CheckForAnyAbility::class,
-        ])->statefulApi();
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- config/auth.php
+++ config/auth.php
@@ -36,7 +36,7 @@ return [
     */
 
     'guards' => [
-        'web' => [
+        'sanctum' => [
             'driver' => 'sanctum',
             'provider' => 'users',
         ],
