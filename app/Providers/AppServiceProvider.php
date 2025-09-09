36a3c4db3afe92fc05dd668c08d2ca3fd26f4e52
jvm2000 <minozajohnvincent2000@gmail.com>
2025-09-10 05:51:18 +0800

    added production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Laravel\Sanctum\Sanctum;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\PersonalAccessToken;
 
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 }
